7f3e38ccfbcf31d52127afb5d4922b4941247c07
los <losiu99@gazeta.pl>
2014-05-07 16:29:25 +0200

    Configured static binding

--- logistics-service-impl/src/main/java/sius/osgi/impl/Activator.java
+++ logistics-service-impl/src/main/java/sius/osgi/impl/Activator.java
@@ -1,5 +1,9 @@
 package sius.osgi.impl;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Properties;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
@@ -10,9 +14,15 @@ public class Activator implements BundleActivator {
 	private ServiceRegistration serviceRegistration;
 
 	public void start(BundleContext context) throws Exception {
+	    Dictionary<String, String> properties = new Hashtable<String, String>();
+	    
+	    properties.put("service.exported.interfaces", "*");
+	    properties.put("service.exported.configs", "org.apache.cxf.ws");
+	    properties.put("org.apache.cxf.ws.address", "http://localhost:9090/logistics");
+	    
 		serviceRegistration = context.registerService(
 				LogisticsService.class.getName(),
-				new LogisticsServiceImpl("none"), null);
+				new LogisticsServiceImpl("http://localhost:9090/logistics"), properties);
 	}
 
 	public void stop(BundleContext context) throws Exception {
